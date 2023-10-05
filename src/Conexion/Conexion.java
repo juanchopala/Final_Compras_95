a5674006588b02e9afacfb004e059ff47e789b16
Juan Cruz Ledesma Pons <juanchelo159@gmail.com>
2023-10-04 21:36:42 -0300

    first commit

---
+++ src/Conexion/Conexion.java
@@ -0,0 +1,40 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mariadb://localhost/comprasdatabase";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER= "org.mariadb.jdbc.Driver";
+    private static Conexion conexion = null;
+    
+    private Conexion(){
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar el driver");
+        }
+    }
+    
+    public static Connection getConexion(){
+        Connection con = null;
+        
+        if (conexion == null){
+            conexion = new Conexion();
+            
+        } try {
+            con = DriverManager.getConnection(URL + "?userLegacyDatatimeCode=false&serverTimezone=UTC" + "&user="+ USUARIO + "&password="+ PASSWORD);
+            //aqui tambien hay una locura
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error de conexion");
+        }
+        return con;
+    }
+                    
+}
---
+++ src/Logica/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+import Conexion.Conexion;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Connection con = Conexion.getConexion();
+        
+    }
+   
+}
