698e0294c3faa739e5541ec018568abbe6b9f739
Juan Cruz Ledesma Pons <juanchelo159@gmail.com>
2023-10-05 13:24:30 -0300

    clases entidades

--- src/Entidades/Producto.java
+++ src/Entidades/Producto.java
@@ -9,5 +9,71 @@ package Entidades;
  * @author Administrador
  */
 public class Producto {
-    
+  private int idProducto;
+  private String nomreProducto;
+  private String descripcion;
+  private double precioActual;
+  private int stock;
+  private boolean estado;
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nomreProducto, String descripcion, double precioActual, int stock, boolean estado) {
+        this.idProducto = idProducto;
+        this.nomreProducto = nomreProducto;
+        this.descripcion = descripcion;
+        this.precioActual = precioActual;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNomreProducto() {
+        return nomreProducto;
+    }
+
+    public void setNomreProducto(String nomreProducto) {
+        this.nomreProducto = nomreProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecioActual() {
+        return precioActual;
+    }
+
+    public void setPrecioActual(double precioActual) {
+        this.precioActual = precioActual;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+  
 }
--- src/Entidades/Proveedor.java
+++ src/Entidades/Proveedor.java
@@ -9,5 +9,51 @@ package Entidades;
  * @author Administrador
  */
 public class Proveedor {
+    private int idProveedor;
+    private String razonSocial;
+    private String domicilio;
+    private String telefono;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int idProveedor, String razonSocial, String domicilio, String telefono) {
+        this.idProveedor = idProveedor;
+        this.razonSocial = razonSocial;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
 }
